2b1ae8371953c800ec10e958385414c00afc0434
Avijeet Pandey <avijeetpandey25@gmail.com>
2021-07-18 17:00:24 +0530

    Add : Create birthday card function

---
+++ app/src/main/java/com/avijeet/birthday_card/BirthdayGreetingActivity.kt
@@ -0,0 +1,11 @@
+package com.avijeet.birthday_card
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+    }
+}
--- app/src/main/java/com/avijeet/birthday_card/MainActivity.kt
+++ app/src/main/java/com/avijeet/birthday_card/MainActivity.kt
@@ -2,10 +2,19 @@ package com.avijeet.birthday_card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name = nameField.editableText.toString()
+        Toast.makeText(this,"name is $name",Toast.LENGTH_LONG).show()
     }
 }
